6f0f7ddf50f26eb764de6b9231fb30378e9d6a2c
shravandevadiga123 <shravanlaptop06@gmail.com>
2026-03-23 19:13:25 +0530

    vercel

--- client/src/pages/AnalyticsDashboard.jsx
+++ client/src/pages/AnalyticsDashboard.jsx
@@ -5,7 +5,7 @@ import {
 } from 'recharts';
 import '../styles/AnalyticsDashboard.css';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 function AnalyticsDashboard() {
   const [analytics, setAnalytics] = useState(null);
--- client/src/pages/GenerateLogPage.jsx
+++ client/src/pages/GenerateLogPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/GenerateLogPage.css';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 const LoadingAnimation = () => {
   const [statusIndex, setStatusIndex] = useState(0);
